40f418a76c3446c5add08b5b37e2c49941f2cf44
prime417 <pankajds417@gmail.com>
2018-08-19 12:29:13 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+void main(){
+  runApp(
+new Container(
+color:Colors.blue,
+child:new Center(
+
+  child:new Directionality(
+
+    textDirection: TextDirection.ltr,
+    child:new Text('Hello, world!')
+  )
+)
+
+)
+
+  );
+}
